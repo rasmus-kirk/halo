a6eb90c3ddc31d59a0d444aa1d5b5d53a3db5e2d
rasmus-kirk <mail@rasmuskirk.com>
2025-03-04 11:37:33 +0100

    fmt

--- plonk/code/benches/bench.rs
+++ plonk/code/benches/bench.rs
@@ -19,7 +19,7 @@ pub fn plonk_proof_verify(c: &mut Criterion) {
     let mut group = c.benchmark_group("plonk_proof_verify");
     for size in MIN..MAX + 1 {
         let out = Arithmetizer::synthesize::<_, 4>(rng, 2usize.pow(size as u32));
-        let input_values = vec![3,4,5,6];
+        let input_values = vec![3, 4, 5, 6];
         let output_wires = &[out];
         let ((x, w), _) = &Arithmetizer::to_circuit(rng, input_values, output_wires).unwrap();
         let pi = plonker::proof(rng, x, w);
@@ -36,15 +36,13 @@ pub fn plonk_proof_verify(c: &mut Criterion) {
         );
     }
     for (i, x, pi) in pis {
-        group.warm_up_time(WARMUP).bench_with_input(
-            BenchmarkId::new("verifier", i),
-            &i,
-            |b, _| {
+        group
+            .warm_up_time(WARMUP)
+            .bench_with_input(BenchmarkId::new("verifier", i), &i, |b, _| {
                 b.iter(|| {
                     plonker::verify(&x, pi.clone());
                 })
-            },
-        );
+            });
     }
     group.finish();
 }
