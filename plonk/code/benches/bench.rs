fe7fab2b40d52e9a3c5fc8261962f60d30647c85
Abdul Haliq <liqo12@hotmail.com>
2025-04-07 08:54:38 +0200

    multi sample bench without criterion, and output csv

--- plonk/code/benches/bench.rs
+++ plonk/code/benches/bench.rs
@@ -1,16 +1,23 @@
-use std::time::{Duration, Instant};
+use std::{
+    env,
+    fs::File,
+    time::{Duration, Instant},
+};
 
+use anyhow::Result;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 const SAMPLE_SIZE: usize = 10;
 const SECONDS: u64 = 2;
 
 use ark_std::test_rng;
+use csv::Writer;
+use itertools::Itertools;
 use log::info;
 use plonk::{arithmetizer::PallasBitArith, pcs::PCSPallas, protocol};
 
 // const WARMUP: Duration = Duration::from_millis(100);
-const MIN: usize = 4;
+const MIN: usize = 13;
 const MAX: usize = 20;
 
 pub fn plonk_proof_verify(c: &mut Criterion) {
@@ -21,50 +28,80 @@ pub fn plonk_proof_verify(c: &mut Criterion) {
 
     // let mut new_pis = Vec::new();
 
-    let mut circuits = Vec::new();
-    println!("|‾‾‾‾|‾‾‾‾‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾‾‾‾‾|");
-    println!("| n  | gen_circ (s) | to_circ (s)  | Prover (s)   | Verifier (s) |");
-    println!("|====|==============|==============|==============|==============|");
-    for size in MIN..MAX + 1 {
-        let d = 2usize.pow(size as u32) - 1;
-        let input_values = vec![3, 4, 5, 6];
-
-        info!("A1");
-        let start_time = Instant::now();
-        let output_wires = &PallasBitArith::synthesize::<4, _>(rng, 2usize.pow(size as u32) - 2);
-        let rand_circuit_time = start_time.elapsed().as_secs_f32();
-        info!("lens: {:?}, {:?}", output_wires.len(), output_wires[0].id());
-
-        info!("A2");
-        let start_time = Instant::now();
-        let (x, w) = &PallasBitArith::to_circuit::<_, _, PCSPallas>(
-            rng,
-            input_values,
-            output_wires,
-            Some(d),
-        )
-        .unwrap();
-        let to_circuit_time = start_time.elapsed().as_secs_f32();
-
-        info!("A3");
-        circuits.push((size, x.clone(), w.clone()));
-
-        let start_time = Instant::now();
-        let new_pi = protocol::prove::<_, _, PCSPallas>(rng, &x, &w);
-        let new_p_time = start_time.elapsed().as_secs_f32();
-        info!("D");
-        // new_pis.push(new_pi.clone());
-
-        let start_time = Instant::now();
-        protocol::verify(&x, new_pi).unwrap();
-        let new_v_time = start_time.elapsed().as_secs_f32();
-
-        println!(
-            "| {:02} | {:>12.8} | {:>12.8} | {:>12.8} | {:>12.8} |",
-            size, rand_circuit_time, to_circuit_time, new_p_time, new_v_time
-        );
+    // let mut circuits = Vec::new();
+    const SAMPLE_SIZE: usize = 6;
+    let mut data: Vec<Vec<f32>> = Vec::with_capacity((MAX - MIN + 1) * 4);
+    for _ in 0..(MAX - MIN + 1) * 4 {
+        data.push(Vec::with_capacity(SAMPLE_SIZE))
     }
-    println!("|____|______________|______________|______________|______________|");
+    for i in 0..SAMPLE_SIZE {
+        println!("Sample {} / {}", i + 1, SAMPLE_SIZE);
+        println!("|‾‾‾‾|‾‾‾‾‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾‾‾‾‾|");
+        println!("| n  | gen_circ (s) | to_circ (s)  | Prover (s)   | Verifier (s) |");
+        println!("|====|==============|==============|==============|==============|");
+        for size in MIN..MAX + 1 {
+            let d = 2usize.pow(size as u32) - 1;
+            let input_values = vec![3, 4, 5, 6];
+
+            let off = (size - MIN) * 4;
+            info!("A1");
+            let start_time = Instant::now();
+            let output_wires = &PallasBitArith::synthesize::<4, _>(rng, d);
+            let rand_circuit_time = start_time.elapsed().as_secs_f32();
+            data[off].push(rand_circuit_time);
+            info!("lens: {:?}, {:?}", output_wires.len(), output_wires[0].id());
+
+            info!("A2");
+            let start_time = Instant::now();
+            let (x, w) = &PallasBitArith::to_circuit::<_, _, PCSPallas>(
+                rng,
+                input_values,
+                output_wires,
+                Some(d),
+            )
+            .unwrap();
+            let to_circuit_time = start_time.elapsed().as_secs_f32();
+            data[off + 1].push(to_circuit_time);
+
+            info!("A3");
+            // circuits.push((size, x.clone(), w.clone()));
+
+            let start_time = Instant::now();
+            let new_pi = protocol::prove::<_, _, PCSPallas>(rng, &x, &w);
+            let new_p_time = start_time.elapsed().as_secs_f32();
+            data[off + 2].push(new_p_time);
+            info!("D");
+            // new_pis.push(new_pi.clone());
+
+            let start_time = Instant::now();
+            protocol::verify(&x, new_pi).unwrap();
+            let new_v_time = start_time.elapsed().as_secs_f32();
+            data[off + 3].push(new_v_time);
+
+            println!(
+                "| {:02} | {:>12.8} | {:>12.8} | {:>12.8} | {:>12.8} |",
+                size, rand_circuit_time, to_circuit_time, new_p_time, new_v_time
+            );
+        }
+        println!("|____|______________|______________|______________|______________|");
+    }
+
+    let sqrt_size = (SAMPLE_SIZE as f32).sqrt();
+    data = data
+        .iter()
+        .flat_map(|xs| {
+            let n = SAMPLE_SIZE as f32;
+            let mean = xs.iter().sum::<f32>() / n;
+            let variance = xs.iter().map(|&x| (x - mean).powi(2)).sum::<f32>() / n;
+            let error = variance.sqrt() / sqrt_size;
+            [mean, error]
+        })
+        .chunks(8)
+        .into_iter()
+        .map(|chunk| chunk.collect())
+        .collect();
+
+    let _ = write_csv(&data);
     // for (i, x, w) in &circuits {
     //     group.warm_up_time(WARMUP).bench_with_input(
     //         BenchmarkId::new("prover", i),
@@ -111,6 +148,39 @@ pub fn plonk_proof_verify(c: &mut Criterion) {
     //     );
     // }
     group.finish();
+}
+
+fn write_csv(data: &Vec<Vec<f32>>) -> Result<()> {
+    // Get the current directory (project root)
+    let current_dir = env::current_dir()?;
+
+    // Construct the full file path
+    let path = current_dir.join("target/bench_data.csv");
+
+    // Create the file and write to it
+    let file = File::create(path)?;
+    let mut wtr = Writer::from_writer(file);
+
+    wtr.write_record(&[
+        "n",
+        "gen_circ",
+        "gen_circ_err",
+        "to_circ",
+        "to_circ_err",
+        "Prover",
+        "Prover_err",
+        "Verifier",
+        "Verifier_err",
+    ])?;
+    for (i, row) in data.into_iter().enumerate() {
+        let n = i + MIN;
+        let mut row_with_n = vec![n as f32];
+        row_with_n.extend(row);
+        wtr.serialize(row_with_n)?;
+    }
+
+    wtr.flush()?;
+    Ok(())
 }
 
 criterion_group! {
